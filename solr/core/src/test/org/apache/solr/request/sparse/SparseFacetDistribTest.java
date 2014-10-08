9d073f88dc58f29c3018e26f9541197c5cb98f8e
Dmitry Kan <dmitry.kan@alpha-sense.com>
2014-10-08 16:47:30 +0300

    #23 tests for sparse faceting

---
+++ solr/core/src/test/org/apache/solr/request/sparse/SparseFacetDistribTest.java
@@ -0,0 +1,213 @@
+package org.apache.solr.request.sparse;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.util.LuceneTestCase;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.cloud.AbstractFullDistribZkTestBase;
+import org.apache.solr.common.params.CommonParams;
+import org.apache.solr.common.params.FacetParams;
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.junit.BeforeClass;
+
+// We need SortedDoc multi value DocValues
+@LuceneTestCase.SuppressCodecs({"Lucene3x", "Lucene40", "Lucene41", "Lucene42", "Appending"})
+public class SparseFacetDistribTest extends AbstractFullDistribZkTestBase {
+  private boolean initPerformed = false;
+
+  @BeforeClass
+  public static void beforeSuperClass() {
+    schemaString = "schema-sparse.xml";      // we need a string id
+  }
+
+  public SparseFacetDistribTest() {
+    super();
+    fixShardCount = true;
+    shardCount = 3;
+    sliceCount = 2;
+  }
+
+  // We override this to avoid the chaos monkey as it sometimes turns off DocValues support
+  @Override
+  protected void initCloud() throws Exception {
+    if (initPerformed) {
+      return;
+    }
+    try {
+      cloudClient = createCloudClient(DEFAULT_COLLECTION);
+      cloudClient.connect();
+      initPerformed = true;
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void doTest() throws Exception {
+    final String FF = "foo_dvm_s";
+    final String THIN = "thin_dvm_s";
+    final int DOCS = 1000;
+
+    handle.clear();
+    handle.put("QTime", SKIPVAL);
+    handle.put("timestamp", SKIPVAL);
+
+    waitForRecoveriesToFinish(false);
+
+    // Add a bunch of documents with a mix of shared and unique values in the facet field
+    List<String> fields = new ArrayList<>();
+    for (int docID = 1 ; docID <= DOCS ; docID++) {
+      fields.clear();
+      fields.add("id"); fields.add(Integer.toString(docID));
+      fields.add(FF);   fields.add("unique" + docID);
+      fields.add(FF);   fields.add("alldocs");
+
+      fields.add(THIN); fields.add("thin" + random().nextInt(DOCS/2));
+      int extra = random().nextInt(10);
+      for (int i = 0 ; i < extra ; i++) {
+        fields.add(FF); fields.add("extra" + random().nextInt(100));
+      }
+
+      String[] fieldsS = new String[fields.size()];
+      fields.toArray(fieldsS);
+      index(fieldsS);
+    }
+    commit();
+
+    // Trigger a two-phase distributed faceting call
+    ModifiableSolrParams params = new ModifiableSolrParams();
+    params.add("q", "*:*");
+    for (int clientID = 0 ; clientID < shardCount ; clientID++) {
+      QueryResponse results = clients.get(clientID).query(params);
+      assertEquals(DOCS, results.getResults().getNumFound());
+    }
+
+    params.add(FacetParams.FACET, Boolean.TRUE.toString());
+    params.add(FacetParams.FACET_FIELD, FF);
+    params.add(FacetParams.FACET_LIMIT, Integer.toString(10));
+    params.add(FacetParams.FACET_METHOD, FacetParams.FACET_METHOD_fc);
+    params.set(SparseKeys.SPARSE, Boolean.FALSE.toString());
+
+    QueryResponse nonSparse = clients.get(0).query(params);
+    QueryResponse nonSparse2 = clients.get(0).query(params);
+    assertEquals("Repeating the facet request with standard Solr should be equal except for QTime",
+        nonSparse.toString().replaceAll("QTime=[0-9]+", ""), nonSparse2.toString().replaceAll("QTime=[0-9]+", ""));
+
+    params.set(SparseKeys.SPARSE, Boolean.TRUE.toString());
+    params.add(SparseKeys.STATS_RESET, Boolean.TRUE.toString());
+    params.add(SparseKeys.TERMLOOKUP, Boolean.TRUE.toString());
+
+    // TODO: Remove these when error has been isolated
+    params.set(SparseKeys.MAXMINCOUNT, Integer.toString(0)); // Let sparse behave like non-sparse
+    params.set(SparseKeys.CACHE_DISTRIBUTED, Boolean.FALSE.toString()); // Caching seems to give incorrect results
+    // TOD: TERMLOOKUP seems the likely culprit for failing test. Maybe it has something to do with bigterms?
+    params.set(SparseKeys.TERMLOOKUP, Boolean.FALSE.toString()); // Third attempt to isolate the problem
+
+    params.add(SparseKeys.MINTAGS, Integer.toString(0));   // Force sparse
+    params.add(SparseKeys.FRACTION, Double.toString(1000.0)); // Force sparse
+    params.add(SparseKeys.CUTOFF, Double.toString(2.0));   // Force sparse
+
+    QueryResponse results = clients.get(0).query(params);
+
+    // Comparison-tests are bad as they need the old implementation to stick around
+    assertEquals("Solr fc and sparse results should be equal except for QTime",
+        nonSparse.toString().replaceAll("QTime=[0-9]+", ""), results.toString().replaceAll("QTime=[0-9]+", ""));
+
+     // Change back to defaults
+    params.remove(SparseKeys.TERMLOOKUP);
+    params.remove(SparseKeys.CACHE_DISTRIBUTED);
+    params.remove(SparseKeys.MAXMINCOUNT);
+
+    // Check that the call was sparse-processed
+    params.remove(SparseKeys.STATS_RESET);
+    params.add(SparseKeys.STATS, Boolean.TRUE.toString()); // Enable sparse statistics
+    results = clients.get(0).query(params);
+    // Do we even sparse and does stats work?
+    assertTrue("For sparse faceting there should an instance of 'exceededCutoff=0'\n" + results,
+        results.toString().contains("exceededCutoff=0"));
+    // Was all calls within the cutoff?
+    assertFalse("For sparse faceting there should be no instances of 'exceededCutoff=[1-9]'\n" + results,
+        results.toString().matches(".*exceededCutoff=[1-9].*"));
+
+    // Check that fine-counting is also sparse-counted
+    params.set(FacetParams.FACET_FIELD, THIN);
+    params.set(FacetParams.FACET_LIMIT, Integer.toString(5));
+
+    // We need to do this twice to get dist stats to bubble up
+    clients.get(0).query(params);
+    results = clients.get(0).query(params);
+
+    // terms(count=0 means that the secondary fine-counting of facets was not done sparsely
+    assertFalse("With fine-counting there should be no instances of 'fallback=0'\n" + results,
+        results.toString().contains("terms(fallback=0"));
+    assertTrue("At least one of the requests should hit the cache\n" + results,
+        results.toString().matches(".*hits=[1234].*"));
+
+    // Is it possible to turn the distributed facet call cache off?
+    params.set(SparseKeys.CACHE_DISTRIBUTED, Boolean.FALSE.toString());
+    params.set(SparseKeys.STATS_RESET, Boolean.TRUE.toString());
+    clients.get(0).query(params);
+    params.remove(SparseKeys.STATS_RESET);
+    clients.get(0).query(params);
+    results = clients.get(0).query(params);
+    assertFalse("With disabled cache, there should be no cache hits\n" + results,
+        results.toString().matches(".*hits=[1234].*"));
+    params.set(SparseKeys.CACHE_DISTRIBUTED, Boolean.TRUE.toString());
+
+    // Disable fine-counting
+    params.set(SparseKeys.STATS_RESET, Boolean.TRUE.toString());
+    params.set(SparseKeys.SKIPREFINEMENTS, Boolean.TRUE.toString());
+    clients.get(0).query(params);
+    params.remove(SparseKeys.STATS_RESET);
+    results = clients.get(0).query(params);
+    assertTrue("Without fine-counting there should be an instance of 'terms(count=0'\n" + results,
+        results.toString().contains("terms(count=0"));
+
+    // minCount 0 vs. minCount 1, both should still be sparse
+    params.set(CommonParams.Q, "id:1");
+    params.set(SparseKeys.STATS_RESET, Boolean.TRUE.toString());
+    params.remove(SparseKeys.SKIPREFINEMENTS);
+    params.set(SparseKeys.MAXMINCOUNT, Integer.toString(0));
+    clients.get(0).query(params);
+    params.remove(SparseKeys.STATS_RESET);
+    results = clients.get(0).query(params);
+    // With tiny result sets all possible facet values are returned in the first call, so no secondary call is needed
+    assertTrue("For single-hit search, setting maxMin=0 should contain 'terms(count=0'\n" + results,
+        results.toString().contains("terms(count=0"));
+    assertTrue("For single-hit search, setting maxMin=0 should contain empty facet values\n" + results,
+        results.toString().matches(".*thin[0-9]+=0.*"));
+
+    /*   TODO: Add facet content with 12+ unique values and a matching search that contains zero values
+    params.set(SparseKeys.MAXMINCOUNT, Integer.toString(1));
+    params.set(SparseKeys.STATS_RESET, Boolean.TRUE.toString());
+    clients.get(0).query(params);
+    params.remove(SparseKeys.STATS_RESET);
+    results = clients.get(0).query(params);
+    // With tiny result sets all possible facet values are returned in the first call, so no secondary call is needed
+    assertFalse("For single-hit search, setting maxMin=1 should not contain 'terms(count=0'\n" + results,
+        results.toString().contains("terms(count=0"));
+    assertFalse("For single-hit search, setting maxMin=1 should not contain empty facet values\n" + results,
+        results.toString().matches(".*thin[0-9]+=0.*"));
+        */
+
+  }
+}
---
+++ solr/core/src/test/org/apache/solr/request/sparse/SparseFacetTest.java
@@ -0,0 +1,196 @@
+package org.apache.solr.request.sparse;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.lucene.util.LuceneTestCase;
+import org.apache.solr.SolrTestCaseJ4;
+import org.apache.solr.common.params.FacetParams;
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.apache.solr.request.SolrQueryRequest;
+import org.junit.BeforeClass;
+
+
+@LuceneTestCase.SuppressCodecs({"Lucene3x", "Lucene40", "Lucene41", "Lucene42", "Appending"})
+public class SparseFacetTest extends SolrTestCaseJ4 {
+
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    // *_dvm_s: Multi-valued DocValues String fields
+    // *_dvs_s: Single-valued DocValues String fields
+    initCore("solrconfig.xml","schema-sparse.xml");
+    createIndex();
+  }
+
+  static int random_commit_percent = 30;
+  static void randomCommit(int percent_chance) {
+    if (random().nextInt(100) <= percent_chance)
+      assertU(commit());
+  }
+
+  static ArrayList<String[]> pendingDocs = new ArrayList<>();
+
+  // committing randomly gives different looking segments each time
+  static void add_doc(String... fieldsAndValues) {
+      pendingDocs.add(fieldsAndValues);
+  }
+
+
+  static void createIndex() throws Exception {
+    indexFacetValues();
+
+    Collections.shuffle(pendingDocs, random());
+    for (String[] doc : pendingDocs) {
+      assertU(adoc(doc));
+      randomCommit(random_commit_percent);
+    }
+    assertU(commit());
+    if (random().nextInt(5) == 1) { // Random test against a fully optimized index
+      assertU(optimize());
+    }
+  }
+
+  public static final String SINGLE_DV_FIELD = "single_dvm_s";
+  public static final String SINGLE_TEXT_FIELD = "single_s1";
+  public static final String MULTI_DV_FIELD = "multi_dvm_s";
+  public static final String MULTI_TEXT_FIELD = "multi_s";
+
+  public static final String MODULO_FIELD = "mod_dvm_s";
+  static final int DOCS = 100;
+  static final int UNIQ_VALUES = 10;
+  static final int MAX_MULTI = 10;
+  static final int[] MODULOS = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50};
+
+  static void indexFacetValues() {
+
+    List<String> values = new ArrayList<>();
+    for (int docID = 0 ; docID < DOCS ; docID++) {
+      values.clear();
+      values.add("id"); values.add(Integer.toString(docID));
+      for (int mod: MODULOS) {
+        if (mod < docID && docID % mod == 0) {
+          values.add(MODULO_FIELD); values.add("mod_" + mod);
+        }
+      }
+      values.add(SINGLE_DV_FIELD); values.add("single_dv_" + random().nextInt(UNIQ_VALUES));
+      values.add(SINGLE_TEXT_FIELD); values.add("single_text_" + random().nextInt(UNIQ_VALUES));
+      final int MULTIS = random().nextInt(MAX_MULTI);
+      for (int i = 0 ; i < MULTIS ; i++) {
+        final String val = i == 0 ? "multi_uniq_" + docID : "multi_" + random().nextInt(UNIQ_VALUES);
+        values.add(MULTI_DV_FIELD); values.add(val);
+        values.add(MULTI_TEXT_FIELD); values.add(val);
+      }
+      add_doc(values.toArray(new String[values.size()]));
+    }
+  }
+
+  public void testSimpleSearch() {
+    assertQ("Match all (*:*) should work",
+        req("*:*"),
+        "//*[@numFound='" + DOCS + "']"
+        );
+
+    assertQ("Modulo 7 search should work",
+        req(MODULO_FIELD + ":mod_7"),
+        "//*[@numFound='" + (DOCS/7-1) + "']"
+    );
+
+  }
+
+  public void testSingleDocValueFaceting() throws Exception {
+    for (int mod: MODULOS) {
+      assertFacetEquality("Modulo check", MODULO_FIELD + ":mod_" + mod, SINGLE_DV_FIELD);
+    }
+    //dumpStats();
+  }
+
+  public void testMultiDocValueFaceting() throws Exception {
+    for (int mod: MODULOS) {
+      assertFacetEquality("Modulo check", MODULO_FIELD + ":mod_" + mod, MULTI_DV_FIELD);
+    }
+  }
+
+  public void testSingleTextValueFaceting() throws Exception {
+    for (int mod: MODULOS) {
+      assertFacetEquality("Modulo check", MODULO_FIELD + ":mod_" + mod, SINGLE_TEXT_FIELD);
+    }
+  }
+
+  public void testMultiTextValueFaceting() throws Exception {
+    for (int mod: MODULOS) {
+      assertFacetEquality("Modulo check", MODULO_FIELD + ":mod_" + mod, MULTI_TEXT_FIELD);
+    }
+  }
+
+  private void dumpStats() throws Exception {
+    SolrQueryRequest req = req("*:*");
+    ModifiableSolrParams params = new ModifiableSolrParams(req.getParams());
+    req.setParams(params);
+    params.set(FacetParams.FACET, true);
+    params.set(FacetParams.FACET_FIELD, SINGLE_DV_FIELD);
+    params.set(SparseKeys.STATS, true);
+    params.set("indent", true);
+    String output = h.query(req).replaceAll("QTime\">[0-9]+", "QTime\">");
+    System.out.println(output);
+  }
+
+  private void assertFacetEquality(String message, String query, String facetField) throws Exception {
+    SolrQueryRequest req = req(query);
+    ModifiableSolrParams params = new ModifiableSolrParams(req.getParams());
+    params.set(FacetParams.FACET, true);
+    params.set(FacetParams.FACET_FIELD, facetField);
+    params.set("indent", true);
+
+    params.set(SparseKeys.SPARSE, false);
+    req.setParams(params);
+    String plain = h.query(req).replaceAll("QTime\">[0-9]+", "QTime\">");
+    params.set(SparseKeys.SPARSE, true);
+    req.setParams(params);
+    String sparse = h.query(req).replaceAll("QTime\">[0-9]+", "QTime\">");
+
+    assertEquals(message + " sparse faceting with query " + query + " should match plain Solr",
+        plain, sparse);
+  }
+
+/*    assertQ("check counts for facet queries",
+            req("q", "id:[42 TO 47]"
+                ,"facet", "true"
+                ,"facet.query", "trait_s:Obnoxious"
+                ,"facet.query", "id:[42 TO 45]"
+                ,"facet.query", "id:[43 TO 47]"
+                ,"facet.field", "trait_s"
+                )
+            ,"*[count(//doc)=6]"
+ 
+            ,"//lst[@name='facet_counts']/lst[@name='facet_queries']"
+            ,"//lst[@name='facet_queries']/int[@name='trait_s:Obnoxious'][.='2']"
+            ,"//lst[@name='facet_queries']/int[@name='id:[42 TO 45]'][.='4']"
+            ,"//lst[@name='facet_queries']/int[@name='id:[43 TO 47]'][.='5']"
+ 
+            ,"//lst[@name='facet_counts']/lst[@name='facet_fields']"
+            ,"//lst[@name='facet_fields']/lst[@name='trait_s']"
+            ,"*[count(//lst[@name='trait_s']/int)=4]"
+            ,"//lst[@name='trait_s']/int[@name='Tool'][.='2']"
+            ,"//lst[@name='trait_s']/int[@name='Obnoxious'][.='2']"
+            ,"//lst[@name='trait_s']/int[@name='Pig'][.='1']"
+            );
+   */
+}
