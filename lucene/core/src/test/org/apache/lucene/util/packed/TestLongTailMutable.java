b9cb38e4aa1d4d23fcc176a51f5724217118409b
Toke Eskildsen <te@ekot.dk>
2015-02-27 15:50:10 +0100

    Corrected the test (the histogram was reversed)

--- lucene/core/src/test/org/apache/lucene/util/packed/TestLongTailMutable.java
+++ lucene/core/src/test/org/apache/lucene/util/packed/TestLongTailMutable.java
@@ -20,25 +20,62 @@ package org.apache.lucene.util.packed;
 import org.apache.lucene.util.LuceneTestCase;
 import org.apache.lucene.util.LuceneTestCase.Slow;
 
+import java.util.Locale;
+
 @Slow
 public class TestLongTailMutable extends LuceneTestCase {
 
   private final static int M = 1048576;
   public void testEstimate() {
-    LongTailMutable.Estimate estimate = new LongTailMutable.Estimate(100*M, getSampleHistogram());
+    final long VC = 400*M;
+    LongTailMutable.Estimate estimate = new LongTailMutable.Estimate(VC, getSampleHistogram());
     for (int tailBPV = 0 ; tailBPV < 64 ; tailBPV++) {
-      System.out.println(String.format("tailBPV=%2d fraction=%4.2f", tailBPV, estimate.getFractionEstimate(tailBPV)));
+      if (estimate.isViable(tailBPV) && estimate.getFractionEstimate(tailBPV) < 1.1) {
+        System.out.println(String.format(Locale.ENGLISH,
+            "tailBPV=%2d men=%4.2fGB/%4.2fGB fraction=%4.2f headValueCount=%4d (%6.4f%%)",
+            tailBPV, estimate.getMemEstimate(tailBPV) / 1024.0 / 1024 / 1024,
+            0.400*estimate.getMaxBPV()/8,
+            estimate.getFractionEstimate(tailBPV), estimate.getHeadValueCount(tailBPV),
+            estimate.getHeadValueCount(tailBPV)*100.0/VC));
+      }
     }
   }
 
   private long[] getSampleHistogram() {
+
     return new long[] {
+        324916865,
+        57336093,
+        37200810,
+        22636130,
+        12677476,
+        7074694,
+        3830153,
+        2317588,
+        1439165,
+        875950,
+        541195,
+        324288,
+        180056,
+        74934,
+        26037,
+        5630,
+        2363,
+        643,
+        619,
+        335,
+        99,
+        5,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 // 32-63
+  };
+/*    return new long[] {
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 32-63
         0, 0, 0, 0, 0, 0, 0, 0, // 24-31
         0, 0, 5, 99, 335, 619, 643, 2363, // 16-23
         5630, 26037, 74934, 180056, 324288, 541195, 875950, 1439165, // 8-15
         2317588, 3830153, 7074694, 12677476, 22636130, 37200810, 57336093, 324916865 // 0-7
-    };
+    };   */
   }
 
 }
