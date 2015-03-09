1daf54bb49981652cc5ce8d0c3930171068748cb
Toke Eskildsen <te@ekot.dk>
2015-03-09 14:09:04 +0100

    Ongoing

--- lucene/core/src/java/org/apache/lucene/util/packed/LongTailBitPlaneMutable.java
+++ lucene/core/src/java/org/apache/lucene/util/packed/LongTailBitPlaneMutable.java
@@ -27,9 +27,9 @@ import java.util.List;
 
 /**
  * Highly experimental structure for holding counters with known maxima.
- * The structure works best with long tail distributes maxima, although
- * the ordering of the maxima is assumed to be random. For sorted maxima,
- * a structure of a little less than half the size is possible.
+ * The structure works best with long tail distributed maxima. The order
+ * of the maxima are assumed to be random. For sorted maxima, a structure
+ * of a little less than half the size is possible.
  * </p><p>
  * Space saving is prioritized very high, while performance is prioritized
  * very low. Practical usage of the structure is thus limited.
@@ -41,6 +41,25 @@ public class LongTailBitPlaneMutable extends PackedInts.Mutable {
 
   private final Plane[] planes;
 
+  /**
+   * The bits needed for a counter are divided among 1 or more planes.
+   * Each plane holds n bits, where n > 0.
+   * Besides the bits themselves, each plane (except the last) holds 1 bit for each value,
+   * signalling if the value overflows onto the next plane.
+   * </p><p>
+   * Example:
+   * There are 5 counters, where the max for the first counter is 10, the max for the second
+   * counter is 1 and the maxima for the rest are 16, 2 and 3. The bits needed to hold the
+   * first counter is 4, since {@code 2^3-1 < 10 < 2^4-1}. The bits needed for the rest of
+   * the values are 1, 5, 2 and 2.<br/>
+   * plane(0) holds the 2 least significant bits (bits 0+1), plane(1) holds bits 2+3, plane(2) holds bit 4.<br/>
+   * plane(0) holds 5 * 2 value bits + 5 * 1 overflow-bit.<br/>
+   * plane(1) holds 2 * 1 value bits + 2 * 1 overflow-bit.<br/>
+   * plane(2) holds 1 * 1 value bits and no overflow bits, since it is the last one.<br/>
+   * Stepping through the maxima-bits 4, 1, 5, 2 and 2, we have<br/>
+   * plane(0).overflowBits = 1 0 1 0 0<br/>
+   * plane(1).overflowBits = 0 1
+   */
   private class Plane {
     private final PackedInts.Mutable values;
     private final OpenBitSet overflows;
@@ -82,11 +101,10 @@ public class LongTailBitPlaneMutable extends PackedInts.Mutable {
       lPlanes.add(new Plane((int) histogram[bit], 1 + extraBitsCount, bit < maxBit, overflowBucketSize, bit));
     }
     planes = lPlanes.toArray(new Plane[lPlanes.size()]);
-    populateStaticStructures(maxima, histogram);
+    populateStaticStructures(maxima);
   }
 
-
-  private void populateStaticStructures(PackedInts.Reader maxima, long[] histogram) {
+  private void populateStaticStructures(PackedInts.Reader maxima) {
     final int[] overflowIndex = new int[planes.length];
     int bit = 0;
     for (int planeIndex = 0; planeIndex < planes.length-1; planeIndex++) { // -1: Never set overflow bit on topmost
@@ -102,19 +120,14 @@ public class LongTailBitPlaneMutable extends PackedInts.Mutable {
           // Update cache
           final int cacheIndex = overflowIndex[planeIndex]/plane.overflowBucketSize;
           if (overflowIndex[planeIndex] % plane.overflowBucketSize == 0) {
-            plane.overflowCache.set(cacheIndex, plane.overflowCache.get(cacheIndex-1)+1);
+            plane.overflowCache.set(cacheIndex, plane.overflowCache.get(cacheIndex-1)+1); // Transfer previous sum
           } else {
-            plane.overflowCache.set(cacheIndex, plane.overflowCache.get(cacheIndex));
+            plane.overflowCache.set(cacheIndex, plane.overflowCache.get(cacheIndex)+1);
           }
         }
       }
       bit += plane.values.getBitsPerValue();
     }
-  }
-
-
-  public LongTailBitPlaneMutable(long[] shift, int overflowBucketSize) {
-    this(new Packed64(1, 2), 3); // TODO: Implement this
   }
 
   private int getMaxBit(long[] histogram) {
