4d8886a36cf9e0027c933d5ff1c51a9360d76ce6
Toke Eskildsen <te@ekot.dk>
2018-07-02 22:21:26 +0200

    Completely untested code for building the cache for the IndexedDISI

--- lucene/core/src/java/org/apache/lucene/codecs/lucene70/IndexedDISICache.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene70/IndexedDISICache.java
@@ -29,6 +29,7 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import org.apache.lucene.store.IndexInput;
+import org.apache.lucene.util.ArrayUtil;
 
 import static org.apache.lucene.codecs.lucene70.IndexedDISI.MAX_ARRAY_LENGTH;
 
@@ -45,31 +46,27 @@ import static org.apache.lucene.codecs.lucene70.IndexedDISI.MAX_ARRAY_LENGTH;
  * Note: There are 4 types of blocks: ALL, DENSE, SPARSE and non-existing (0 set bits).
  * Non-existing blocks are marked with offset -1.
  *
- * The rank structure for DENSE blocks is an array of {@link long}s with an entry for each
- * sub-block of 2048 bits out of the 65536 bits.
- * Each long is logically divided:
- * The first 32 bits contains the total number of set bits up until before the start of the
- * sub-block in the full IndexedDISI bitmap. Masking to the lower 16 bits gives the total
- * number of set bits up up until before the start of the sub-block within the 65536 bit block.
- * Following this are 3*10 bits, stating the number of set bits within the first three 512 bit
- * sub-sub-blocks in the 2048 bit sub-block.
- * Getting the number of set bits for address 65535 (worst-case) for a DENSE sub-block:
- * 1) Retrieve the {@code long} rankEntry at {@code rank[31]}
- * 2) Set {@code bitCount = rankEntry >>> 32}
- * 3) Set {@code bitCount += rankEntry >>> 22 & 1023 + rankEntry >>> 12 & 1023 + rankEntry >>> 2 & 1023}
- * 4) Set {@code bitCount += Long.bitCount(block[index])} with index 1016...1023
+ * The rank structure for DENSE blocks is an array of unsigned {@code short}s with an entry
+ * or each sub-block of 512 bits out of the 65536 bits in the outer block.
+ * Each rank-entry states the number of set bits within the block up to the bit before the
+ * bit positioned at the start of the sub-block.
+ * Note that that the rank entry of the first sub-block is always 0 and that the last entry can
+ * at most be 65536-512 = 65024 and thus will always fit into an unsigned short.
  * See https://en.wikipedia.org/wiki/Succinct_data_structure for details on rank structures.
  * The alternative to using the rank structure is iteration and summing of set bits for all
  * entries in the DENSE sub-block up until the wanted bit, with a worst-case of 1024 entries.
- * The rank cache overhead for a single DENSE block is 32 longs or 1/32th of the sub-block size.
+ * The rank cache overhead for a single DENSE block is 128 shorts (128*16 = 2048 bits) or
+ * 1/32th.
  *
  * The total overhead for the rank cache is currently also numDocs/32 bits or numDocs/8 bytes
  * as the rank-representation is not sparse itself, using empty entries for sub-blocks of type
  * ALL or SPARSE. // TODO: Support sparse rank structures
  */
 public class IndexedDISICache {
-  private final long[] blockOffsets;
-  private final long[] rank;
+  private static final int RANKS_PER_BLOCK = 65536/512;
+
+  private long[] blockOffsets = null; // One every 65536 docs, contains slice position
+  private char[] rank;        // One every 512 docs
 
   /**
    * Builds the stated caches for the given Indexed
@@ -77,179 +74,106 @@ public class IndexedDISICache {
    * @param in positioned at the start of the logical underlying bitmap.
    */
   IndexedDISICache(IndexInput in, int numDocs, boolean createBlockCache, boolean createRankCache) throws IOException {
-    blockOffsets = createBlockCache ? new long[numDocs >> 16] : null; // One every 65536 docs
-    rank = createRankCache ? new long[numDocs >> 11] : null;           // One every 2048 docs
+    if (createBlockCache) {
+      blockOffsets = new long[16]; // Will be extended when needed
+      Arrays.fill(blockOffsets, -1L); // -1 signals empty (the default)
+    }
+    rank = createRankCache ? new char[256] : null; // Will be extended when needed
     if (!createBlockCache && !createRankCache) {
       return; // Nothing to do
     }
 
-    //fillCaches(in, numDocs, createBlockCache, createRankCache);
+    fillCaches(in, createBlockCache, createRankCache);
   }
 
-  enum IndexedMethod {ALL, DENSE, SPARSE }
-  // Extracted and reduced from IndexedDISI
-  private class disiIterator {
-    private final IndexInput slice;
-
-    public disiIterator(IndexInput slice) {
-      this.slice = slice;
-    }
-
-    private int block = -1;
-    private long blockEnd;
-    private int nextBlockIndex = -1;
-    IndexedMethod method;
-
-    private int doc = -1;
-    private int index = -1;
-
-    // DENSE variables
-    private long word;
-    private int wordIndex = -1;
-    // number of one bits encountered so far, including those of `word`
-    private int numberOfOnes;
-
-    // ALL variables
-    private int gap;
-
-    public int docID() {
-      return doc;
-    }
-
-    private void advanceBlock(int targetBlock) throws IOException {
-      do {
-        slice.seek(blockEnd);
-        readBlockHeader();
-      } while (block < targetBlock);
-    }
-
-    private void readBlockHeader() throws IOException {
-      block = Short.toUnsignedInt(slice.readShort()) << 16;
-      assert block >= 0;
-      final int numValues = 1 + Short.toUnsignedInt(slice.readShort());
-      index = nextBlockIndex;
-      nextBlockIndex = index + numValues;
-      if (numValues <= MAX_ARRAY_LENGTH) {
-        method = IndexedMethod.SPARSE;
-        blockEnd = slice.getFilePointer() + (numValues << 1);
-      } else if (numValues == 65536) {
-        method = IndexedMethod.ALL;
-        blockEnd = slice.getFilePointer();
-        gap = block - index - 1;
-      } else {
-        method = IndexedMethod.DENSE;
-        blockEnd = slice.getFilePointer() + (1 << 13);
-        wordIndex = -1;
-        numberOfOnes = index + 1;
-      }
-    }
-
-    public int index() {
-      return index;
-    }
-
-
-    boolean denseAdvanceWithinBlock(int target) throws IOException {
-      final int targetInBlock = target & 0xFFFF;
-      final int targetWordIndex = targetInBlock >>> 6;
-      for (int i = wordIndex + 1; i <= targetWordIndex; ++i) {
-        word = slice.readLong();
-        numberOfOnes += Long.bitCount(word);
-      }
-      wordIndex = targetWordIndex;
-
-      long leftBits = word >>> target;
-      if (leftBits != 0L) {
-        doc = target + Long.numberOfTrailingZeros(leftBits);
-        index = numberOfOnes - Long.bitCount(leftBits);
-        return true;
-      }
-
-      while (++wordIndex < 1024) {
-        word = slice.readLong();
-        if (word != 0) {
-          index = numberOfOnes;
-          numberOfOnes += Long.bitCount(word);
-          doc = block | (wordIndex << 6) | Long.numberOfTrailingZeros(word);
-          return true;
-        }
-      }
-      return false;
-    }
-
-    boolean advanceExactWithinBlock(int target) throws IOException {
-      final int targetInBlock = target & 0xFFFF;
-      final int targetWordIndex = targetInBlock >>> 6;
-      for (int i = wordIndex + 1; i <= targetWordIndex; ++i) {
-        word = slice.readLong();
-        numberOfOnes += Long.bitCount(word);
-      }
-      wordIndex = targetWordIndex;
-
-      long leftBits = word >>> target;
-      index = numberOfOnes - Long.bitCount(leftBits);
-      return (leftBits & 1L) != 0;
-    }
+  /**
+   * If available, returns a position within the underlying {@link IndexInput} for the start of the block
+   * containing the wanted bit (the target) or the next non-EMPTY block, if the block representing the bit is empty.
+   * @param target the docID that shall be resolved.
+   * @return the offset for the block for target or -1 if it cannot be resolved.
+   */
+  public long getFilePointer(int target) {
+    final int blockTarget = target >> 16;
+    return blockOffsets == null || blockOffsets.length <= blockTarget ? -1 : blockOffsets[blockTarget];
   }
 
-  private void fillCaches(IndexInput slice, int numDocs, boolean fillBlockCache, boolean createRankCache)
+  // TODO: Add a method that updates the slice-position and returns doc & index (maybe as a long?
+
+  private void fillCaches(IndexInput slice, boolean fillBlockCache, boolean fillRankCache)
       throws IOException {
-    final int lastBlock = numDocs & 0xFFFF0000;
-    if (fillBlockCache) {
-      Arrays.fill(blockOffsets, -1L); // Default is no set bits
-    }
 
-    int block = -1;
-    long blockOffset = 0;
-    long nextBlockOffset = slice.getFilePointer();
-    int setBits = 0;
+    // Fill phase
+    int largestBlock = -1;
+    while (slice.getFilePointer() < slice.length()) {
+      final long startFilePointer = slice.getFilePointer();
 
-    while (block <= lastBlock) {
-      // Move to next block
-      slice.seek(nextBlockOffset);
-      blockOffset = nextBlockOffset;
-
-      if (fillBlockCache) {
-        blockOffsets[block] = blockOffset;
-      }
-
-    }
-
-
-    int index = 0;
-
-
-    while (index < numDocs) {
-      assert index >> 16 << 16 == 0; // Blocks are 2^16 bits
-      if (fillBlockCache) {
-        blockOffsets[index >> 16] = blockOffset;
-      }
-
-
-// Get basic data
-      final int blockIndex = Short.toUnsignedInt(slice.readShort()) << 16;
-      assert blockIndex == index >> 16;
+      final int blockIndex = Short.toUnsignedInt(slice.readShort());
       final int numValues = 1 + Short.toUnsignedInt(slice.readShort());
-      IndexedMethod method;
-      if (numValues <= MAX_ARRAY_LENGTH) {
-        method = IndexedMethod.SPARSE;
-        nextBlockOffset = slice.getFilePointer() + (numValues << 1);
-      } else if (numValues == 65536) {
-        method = IndexedMethod.ALL;
-        nextBlockOffset = slice.getFilePointer();
-      } else {
-        method = IndexedMethod.DENSE;
-        nextBlockOffset = slice.getFilePointer() + (1 << 13);
+
+      assert blockIndex > largestBlock;
+      largestBlock = blockIndex;
+
+      if (fillBlockCache) {
+        blockOffsets = ArrayUtil.grow(blockOffsets, blockIndex); // No-op if large enough
+        blockOffsets[blockIndex] = startFilePointer;
       }
 
-      if (method != IndexedMethod.DENSE) { // Only DENSE is ranked
-        index += 1 << 16;
+      if (numValues <= MAX_ARRAY_LENGTH) { // SPARSE
+        slice.seek(slice.getFilePointer() + (numValues << 1));
+        continue;
+      }
+      if (numValues == 65536) { // ALL
+        // Already at next block offset
         continue;
       }
 
-      // The block is DENSE, so rank it
+      // The block is DENSE
+      long nextBlockOffset = slice.getFilePointer() + (1 << 13);
+      if (fillRankCache) {
+        int setBits = 0;
+        int rankOrigo = blockIndex << 16 >> 9; // Double shift for clarity: The compiler will simplify it
+        for (int rankDelta = 0 ; rankDelta < RANKS_PER_BLOCK ; rankDelta++) { // 128 rank-entries in a block
+          final int rankIndex = rankOrigo + rankDelta;
+          rank = ArrayUtil.grow(rank, rankIndex);
+          rank[rankIndex] = (char)setBits;
+          for (int i = 0 ; i < 512/64 ; i++) { // 8 longs for each rank-entry
+            setBits += Long.bitCount(slice.readLong());
+          }
+        }
+      }
+      assert slice.getFilePointer() == nextBlockOffset;
     }
 
+    // Reduce & polish phase
+    if (largestBlock == -1) { // No set bit: Disable the caches
+      // TODO: Maybe signal a warning somehow?
+      blockOffsets = null;
+      rank = null;
+      return;
+    }
+
+    if (fillBlockCache && blockOffsets.length-1 > largestBlock) {
+      long[] newBlockOffsets = new long[largestBlock - 1];
+      System.arraycopy(blockOffsets, 0, newBlockOffsets, 0, newBlockOffsets.length);
+      blockOffsets = newBlockOffsets;
+    }
+
+    if (fillBlockCache) {
+      long latestValid = -1;
+      for (int i = blockOffsets.length-1 ; i >= 0 ; i--) {
+        if (blockOffsets[i] == -1) { // -1 means EMPTY block, so skip to the next valid one
+          blockOffsets[i] = latestValid;
+        } else {
+          latestValid = blockOffsets[i];
+        }
+      }
+    }
+
+    if (fillRankCache && rank.length/RANKS_PER_BLOCK > largestBlock) {
+      char[] newRank = new char[largestBlock/RANKS_PER_BLOCK];
+      System.arraycopy(rank, 0, newRank, 0, newRank.length);
+      rank = newRank;
+    }
   }
 
 }
